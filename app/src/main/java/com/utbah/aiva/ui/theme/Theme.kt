7a688e3da850eac072a44cd7875f406f3e1d5007
Utbah <utbah26199@gmail.com>
2026-01-28 00:48:02 +0500

    Adjustment in ui.theme

--- app/src/main/java/com/utbah/aiva/ui/theme/Theme.kt
+++ app/src/main/java/com/utbah/aiva/ui/theme/Theme.kt
@@ -3,7 +3,6 @@ package com.utbah.aiva.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
